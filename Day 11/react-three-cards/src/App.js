e86e507623afb2938532c72bf125d1cb66134c50
Nick <nhowell3@atu.edu>
2025-02-26 14:46:05 -0600

    codealong9

--- Day 11/react-three-cards/src/App.js
+++ Day 11/react-three-cards/src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 
 export default function App(){
   const cards = [
-    {title: "Card 1", content: "First", color: "white"},
-    {title: "Card 2", content: "Second", color: "white"},
-    {title: "Card 3", content: "Third", color: "white"},
+    {title: "Card 1", content: "First", color: "black"},
+    {title: "Card 2", content: "Second", color: "green"},
+    {title: "Card 3", content: "Third", color: "blue"}
   ];
   
   return (
     <div className="container">
       {
-        cards.map((card, index) => {
+        cards.map((card, index) => (
           <Card key={index}{...card}/>
-        })
+        ))
       }
     </div>
   );
