21ed8e2b87449ebf0ca411b4fe1b35a7b267ca2e
Nick <nhowell3@atu.edu>
2025-03-19 13:46:59 -0500

    rest of code

--- Day 16/server.js
+++ Day 16/server.js
@@ -19,3 +19,7 @@ app.use((err, req, res, next) => {
     res.status(500).send({error: 'Internal Server Error'});
 });
 
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
